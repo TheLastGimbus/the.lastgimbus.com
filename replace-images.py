949614553f399afa8628ac860ca867c4854e12c3
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-12-21 20:09:11 +0100

    script to replace html images with standard ones

---
+++ replace-images.py
@@ -0,0 +1,30 @@
+import sys
+
+file = sys.argv[1]
+
+# find all <img> tags in a file, and replace them with standard markown ![alt](src) tags
+# ignore width, height, and other attributes
+
+with open(file, 'r') as f:
+    text = f.read()
+
+# find all <img> tags
+import re
+pattern = re.compile(r'<img.*?>')
+imgs = pattern.findall(text)
+
+# replace each <img> tag with markdown ![alt](src) tag
+for img in imgs:
+    # find src
+    pattern = re.compile(r'src="(.*?)"')
+    src = pattern.findall(img)[0]
+    # find alt
+    pattern = re.compile(r'alt="(.*?)"')
+    alt = pattern.findall(img)[0]
+    # replace
+    text = text.replace(img, '![{0}]({1})'.format(alt, src))
+
+# write to file
+with open(file, 'w') as f:
+    f.write(text)
+
